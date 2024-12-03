cea20e092728f852267e298bcec730057b44696e
Pelle Wessman <pelle@kodfabrik.se>
2024-12-03 17:20:00 +0100

    test: fix timezone troubles (#5)

--- test/integration/test-helpers.spec.js
+++ test/integration/test-helpers.spec.js
@@ -65,18 +65,18 @@ describe('PgTestHelpers integration', function () {
       (await queryPromise('SELECT * FROM users'))
         .rows.should.deep.equal([
           {
-            'created_at': new Date('2024-12-03T12:53:56.587Z'),
+            'created_at': new Date('2024-12-03T13:53:56.587Z'),
             'email': 'bob@example.com',
             'id': 'c7f1e901-28b0-41d7-9313-80fd10a07e74',
-            'last_edited_at': new Date('2024-12-03T12:53:56.587Z'),
+            'last_edited_at': new Date('2024-12-03T13:53:56.587Z'),
             'name': 'Bob Smith',
             'role': 'superuser',
           },
           {
-            'created_at': new Date('2024-12-03T12:53:56.587Z'),
+            'created_at': new Date('2024-12-03T13:53:56.587Z'),
             'email': 'carl@example.com',
             'id': '2d8d41ba-fcc4-4296-a212-61365e0efb75',
-            'last_edited_at': new Date('2024-12-03T12:53:56.587Z'),
+            'last_edited_at': new Date('2024-12-03 13:53:56.587+00'),
             'name': 'Carl Foo',
             // eslint-disable-next-line unicorn/no-null
             'role': null,
@@ -142,18 +142,18 @@ describe('PgTestHelpers integration', function () {
       (await queryPromise('SELECT * FROM users'))
         .rows.should.deep.equal([
           {
-            'created_at': new Date('2024-12-03T12:53:56.587Z'),
+            'created_at': new Date('2024-12-03T13:53:56.587Z'),
             'email': 'bob@example.com',
             'id': 'c7f1e901-28b0-41d7-9313-80fd10a07e74',
-            'last_edited_at': new Date('2024-12-03T12:53:56.587Z'),
+            'last_edited_at': new Date('2024-12-03T13:53:56.587Z'),
             'name': 'Bob Smith',
             'role': 'superuser',
           },
           {
-            'created_at': new Date('2024-12-03T12:53:56.587Z'),
+            'created_at': new Date('2024-12-03T13:53:56.587Z'),
             'email': 'carl@example.com',
             'id': '2d8d41ba-fcc4-4296-a212-61365e0efb75',
-            'last_edited_at': new Date('2024-12-03T12:53:56.587Z'),
+            'last_edited_at': new Date('2024-12-03T13:53:56.587Z'),
             'name': 'Carl Foo',
             // eslint-disable-next-line unicorn/no-null
             'role': null,
@@ -164,13 +164,13 @@ describe('PgTestHelpers integration', function () {
         .rows.should.deep.equal([
           {
             'bar_id': '026bf711-57fe-4b19-8082-45f8f839a654',
-            'created_at': new Date('2024-12-03T13:38:33.021Z'),
+            'created_at': new Date('2024-12-03T14:38:33.021Z'),
             'foo_id': '727fe786-9bb4-45a5-aba0-de18ac77554a',
             'value': 'Foobar value 1',
           },
           {
             'bar_id': '633b6403-d66b-42c2-8a80-e89dd50301f9',
-            'created_at': new Date('2024-12-03T13:38:33.021Z'),
+            'created_at': new Date('2024-12-03T14:38:33.021Z'),
             'foo_id': '727fe786-9bb4-45a5-aba0-de18ac77554a',
             'value': 'Foobar value 2',
           },
